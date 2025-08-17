6eec05338b52b3383916d1af4d7bed2e57c410ce
OmDev09 <reachomchaudhari@gmail.com>
2025-08-17 20:56:23 +0530

    resume

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -4,15 +4,12 @@ const Navbar = () => {
   return (
     <nav className="navbar">
       <Link to="/">
-          <p className="text-2xl font-bold text-gradient">RESUMIND</p>
+        <p className="text-2xl font-bold text-gradient">RESUMIND</p>
       </Link>
-      <Link to="/upload" className="primaryButton w-fit">
+      <Link to="/upload" className="primary-button w-fit">
         Upload Resume
       </Link>
-
-
     </nav>
-  );
-};
-
-export default Navbar;
+  )
+}
+export default Navbar
--- app/root.tsx
+++ app/root.tsx
@@ -9,8 +9,8 @@ import {
 
 import type { Route } from "./+types/root";
 import "./app.css";
-import { usePuterStore } from '~/lib/puter';
-import { useEffect } from 'react';
+import {usePuterStore} from "~/lib/puter";
+import {useEffect} from "react";
 
 export const links: Route.LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
@@ -26,7 +26,7 @@ export const links: Route.LinksFunction = () => [
 ];
 
 export function Layout({ children }: { children: React.ReactNode }) {
-  const{init} = usePuterStore();
+  const { init } = usePuterStore();
 
   useEffect(() => {
     init()
@@ -34,19 +34,18 @@ export function Layout({ children }: { children: React.ReactNode }) {
 
   return (
     <html lang="en">
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <Meta />
-        <Links />
-      </head>
-      <body>
-        <script src="https://js.puter.com/v2/"></script>
-
-        {children}
-        <ScrollRestoration />
-        <Scripts />
-      </body>
+    <head>
+      <meta charSet="utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <Meta />
+      <Links />
+    </head>
+    <body>
+    <script src="https://js.puter.com/v2/"></script>
+    {children}
+    <ScrollRestoration />
+    <Scripts />
+    </body>
     </html>
   );
 }
