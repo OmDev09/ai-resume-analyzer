a8934fb3e8c8ad282dd233346292e8abae2d6fb9
OmDev09 <reachomchaudhari@gmail.com>
2025-09-24 12:50:25 +0530

    update title of web app

--- app/routes/auth.tsx
+++ app/routes/auth.tsx
@@ -3,7 +3,7 @@ import {useEffect} from "react";
 import {useLocation, useNavigate} from "react-router";
 
 export const meta = () => ([
-  { title: 'Resumind | Auth' },
+  { title: 'Rexa | Auth' },
   { name: 'description', content: 'Log into your account' },
 ])
 
